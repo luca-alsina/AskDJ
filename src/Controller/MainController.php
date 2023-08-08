28a125f8bea25a280d5d4d0cbec00bb5fe88b672
Luca ALSINA <contact@alsinaluca.fr>
2023-08-08 22:11:23 +0200

    Search track response test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controller;
 
-class MainController
+use App\Service\SpotifyService;
+use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends BaseController
 {
+
+    public function __construct(private readonly SpotifyService $spotifyService)
+    {
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    #[Route('/')]
+    public function index() : Response
+    {
+
+        return $this->jsonResponse($this->spotifyService->searchTrack('Encore un matin'));
+
+    }
 
 }
